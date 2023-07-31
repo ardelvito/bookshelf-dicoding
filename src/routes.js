6bf3fc1d229bb81834438145b103f2aa0946b982
ardelvito <ardelvito8738@gmail.com>
2023-07-31 14:00:27 +0700

    Finished with Optional Features

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBooksHandler, getDetailBooks, updateBook, deleteBook } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getDetailBooks
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBook
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBook
+    }
+]
+
+module.exports = routes;
